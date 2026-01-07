5e3521e109135b11990df2d26565c048d5a6df6e
MuqhtadeerM <muhammedmuzawar9@gmail.com>
2026-01-08 01:09:09 +0530

    learning inheritance

--- types-system/classes.ts
+++ types-system/classes.ts
@@ -1,15 +1,28 @@
 // classes are templates or blue prints to create the objects
 
-class Employee {
-  startShift(): void {
-    console.log("start to shift of employee");
-  }
+// means it defines the properties and methods of the object...
 
-  endshift(): void {
-    console.log("employee shift is ended");
+class Employee {
+  constructor(public age: number) {}
+  protected endShift(): void {
+    console.log("start to shift of employee");
   }
 }
 
-const employee = new Employee();
-employee.startShift();
-employee.endshift();
+const employee = new Employee(20);
+console.log(employee.age);
+
+// class Mechanic extends Employee {
+//   private startShift(): void {
+//     console.log("Machanic started");
+//   }
+
+//   goToWork(): void {
+//     this.startShift();
+//   }
+// }
+
+// const mechanic = new Mechanic();
+// mechanic.goToWork();
+
+// modifiers to acces the privvate data
